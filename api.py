a1631cfb6b4b4f592786dc1d2cfdb98ec920a67c
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2021-09-21 17:15:30 -0300

    - Added API provide to application - Update requirements.txt with modules to provide API

--- api.py
+++ api.py
@@ -1,26 +1,31 @@
 """ api """
-import flask
-import sys
-import os
-import concurrent.futures
+# -*- encoding: utf-8 -*-
 
-from certificate import get_certificate, print_basic_info
-from settings import read_hosts
+import json
+import concurrent.futures
+import flask
+
+
+from flask.wrappers import Response
+from certificate import get_certificate, read_hosts, print_basic_info
 
 
 app = flask.Flask(__name__)
-app.config['DEBUG'] = True
 
 
 @app.route('/api/v1/cert/info', methods=['GET'])
-def cert_info():
-    """ get certificate info """
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as e:
-        for hostinfo in e.map(lambda x: get_certificate(x[0], int(x[1])), read_hosts()):
-            cert_infos = print_basic_info(hostinfo)
+def api_cert_info():
+    """ Return a JSON with the certificate info """
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        cert_list = []
+        for hostinfo in executor.map(lambda x:
+                                     get_certificate(x[0], int(x[1])),
+                                     read_hosts()):
+            cert_list.append(json.loads(print_basic_info(hostinfo)))
 
-    return print(cert_infos)
+    return Response(json.dumps(cert_list, indent=4, default=str),
+                    mimetype='application/json', status=200)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=False, processes=4)
