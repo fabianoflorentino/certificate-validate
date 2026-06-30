e1b5371a730d9d71fe094ef6c8179159cfbc4066
Fabiano Santos Florentino <fabianoratm@gmail.com>
2026-06-30 00:12:06 -0300

    refactor(main): remove redundant comment from main function

--- cmd/certificate-validate/main.go
+++ cmd/certificate-validate/main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/fabianoflorentino/certificate-validate/internal/cmd"
 )
 
-// main is the entry point of the application. It executes the command-line interface (CLI) defined in the cmd package.
 func main() {
 	cmd.Execute()
 }
