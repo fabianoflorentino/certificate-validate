c8169649de4ac7997d6b5855e3c21b3d5604bdc4
Fabiano Santos Florentino <fabianoratm@gmail.com>
2026-06-29 23:01:11 -0300

    fix: update golangci-lint installation path and add comment to main function

--- cmd/certificate-validate/main.go
+++ cmd/certificate-validate/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/fabianoflorentino/certificate-validate/internal/cmd"
 )
 
+// main is the entry point of the application. It executes the command-line interface (CLI) defined in the cmd package.
 func main() {
 	cmd.Execute()
 }
